25da8204924aebba5910c6fe76b3b7709cf9649d
Rimuru-Tempest-26 <pkh2662003@gmail.com>
2023-03-28 00:35:48 +0630

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import _ from 'lodash';
 import './style.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
-function component() {
-  const element = document.createElement('div');
+const list = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'complete todo list project',
+    completed: false,
+    index: 1,
+  },
+];
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const showList = (list) => {
+  const listForm = document.getElementById('todoList');
 
-  return element;
-}
+  list
+    .sort((a, b) => a.index - b.index)
+    .forEach((l) => {
+      listForm.innerHTML += `
+        <li for="${l.index}" class="list-group-item d-flex align-items-center justify-content-between" id="list${l.index}">
+          <div class="d-flex align-items-center gap-3">
+            <input type="checkbox" class="form-check-input" id="${l.index}" ${l.completed ? 'checked' : ''} />
+            <input maxlength="255" class="custom-input" value="${l.description}">
+          </div>
+          <i class="bi bi-three-dots-vertical"></i>
+        </li>`;
+    });
+};
 
-document.body.appendChild(component());
+showList(list);
