b06f129fae619d3467c993d3a61b77678a86f98a
Rimuru-Tempest-26 <pkh2662003@gmail.com>
2023-03-29 23:20:51 +0630

    finish

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: './src/index.html',
-    }),
-  ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  devServer: {
+    static: { directory: path.resolve(__dirname, 'dist') }, port: 3000, open: true, hot: true, compress: true, historyApiFallback: true,
   },
   module: {
     rules: [
@@ -26,7 +20,9 @@ module.exports = {
       },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
